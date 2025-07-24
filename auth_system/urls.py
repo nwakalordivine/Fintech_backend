00c4427953b198a470294de830663198f76da85c
nwakalordivine <johndoe@example.com>
2025-07-24 16:58:18 +0100

    Comments Added For Code Understanding

--- auth_system/urls.py
+++ auth_system/urls.py
@@ -4,13 +4,13 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 # url paths
 urlpatterns = [
-    path('auth/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('auth/register/', views.RegisterCreateView.as_view(), name='register'),
-    path('auth/password/reset-code/', views.PasswordResetView.as_view(), name='email_reset_code'),
-    path('auth/password-reset/', views.PasswordResetConfirmView.as_view(), name='password_reset'),
-    path('profile/update/', views.UserUpdateView.as_view(), name='profile-update'),
-    path('profile/', views.UserProfileView.as_view(), name='user-profile'),
-    path('user/wallet/', views.WalletView.as_view(), name="user_wallet_details"),
+    path('api/auth/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/auth/register/', views.RegisterCreateView.as_view(), name='register'),
+    path('api/auth/password/reset-code/', views.PasswordResetView.as_view(), name='email_reset_code'),
+    path('api/auth/password-reset/', views.PasswordResetConfirmView.as_view(), name='password_reset'),
+    path('api/profile/update/', views.UserUpdateView.as_view(), name='profile-update'),
+    path('api/profile/', views.UserProfileView.as_view(), name='user-profile'),
+    path('api/user/wallet/', views.WalletView.as_view(), name="user_wallet_details"),
     path('/', views.HomeView.as_view(), name='home-view'),
 ]
--- fintech_api/urls.py
+++ fintech_api/urls.py
@@ -21,7 +21,7 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, Spec
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/", include("auth_system.urls")),
+    path("", include("auth_system.urls")),
     path("api/", include("userprofile.urls")),
     path("api/", include("operations.urls")),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
